487d55b44cd30e698ba7477dfbd9f74d2c90f87b
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2022-12-10 13:14:24 +0600

    connected the xml elements to activity class

--- app/src/main/java/com/cse464/floralstore/activities/SignInActivity.java
+++ app/src/main/java/com/cse464/floralstore/activities/SignInActivity.java
@@ -8,19 +8,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.cse464.floralstore.MainActivity;
 import com.cse464.floralstore.R;
 
 public class SignInActivity extends AppCompatActivity {
-    private Button CreateNewAccountButton;
+    Button CreateAccountButton, SignInButton;
+    EditText inputEmail, inputPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
-        CreateNewAccountButton = (Button) findViewById(createAccountButton);
+        CreateAccountButton = findViewById(createAccountButton);
+        SignInButton = findViewById(R.id.SignInButton);
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+
         View.OnClickListener createAccountListener = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,6 +34,6 @@ public class SignInActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         };
-        CreateNewAccountButton.setOnClickListener(createAccountListener);
+        CreateAccountButton.setOnClickListener(createAccountListener);
     }
 }
