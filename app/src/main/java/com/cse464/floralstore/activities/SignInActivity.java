05b8f8f94b9ad6fe618eb6f8f99aab1dea85ea43
Faisal Bhuiyan <faisalbhuiyan3038@gmail.com>
2022-12-10 18:13:29 +0600

    added code for registering and logging in using sqlite

--- app/src/main/java/com/cse464/floralstore/activities/SignInActivity.java
+++ app/src/main/java/com/cse464/floralstore/activities/SignInActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cse464.floralstore.MainActivity;
 import com.cse464.floralstore.R;
@@ -16,6 +17,7 @@ import com.cse464.floralstore.R;
 public class SignInActivity extends AppCompatActivity {
     Button CreateAccountButton, SignInButton;
     EditText inputEmail, inputPassword;
+    DBHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +28,7 @@ public class SignInActivity extends AppCompatActivity {
         SignInButton = findViewById(R.id.SignInButton);
         inputEmail = findViewById(R.id.inputEmail);
         inputPassword = findViewById(R.id.inputPassword);
+        myDB = new DBHelper(this);
 
         View.OnClickListener createAccountListener = new View.OnClickListener() {
             @Override
@@ -35,5 +38,35 @@ public class SignInActivity extends AppCompatActivity {
             }
         };
         CreateAccountButton.setOnClickListener(createAccountListener);
+
+        View.OnClickListener signInListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                String password = inputPassword.getText().toString();
+
+                if (email.equals("") || password.equals("")) {
+                    Toast.makeText(SignInActivity.this, "Please Fill Up All Fields!", Toast.LENGTH_SHORT).show();
+                } else {
+                    if (myDB.SignUpVerification(email) == false) {
+                        Toast.makeText(SignInActivity.this, "User Not Found!", Toast.LENGTH_SHORT).show();
+                        inputEmail.setText("");
+                        inputPassword.setText("");
+                    }
+                    else if (myDB.SignInVerification(email,password) == false) {
+                        Toast.makeText(SignInActivity.this, "Incorrect Credentials! Please Try Again..", Toast.LENGTH_SHORT).show();
+                        inputEmail.setText("");
+                        inputPassword.setText("");
+                    }
+                    else {
+                        Intent intent = new Intent(SignInActivity.this, ProductListingActivity.class);
+                        startActivity(intent);
+                    }
+                }
+
+            }
+        };
+        SignInButton.setOnClickListener(signInListener);
     }
 }
--- app/src/main/java/com/cse464/floralstore/activities/SignUpActivity.java
+++ app/src/main/java/com/cse464/floralstore/activities/SignUpActivity.java
@@ -3,14 +3,72 @@ package com.cse464.floralstore.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cse464.floralstore.R;
 
 public class SignUpActivity extends AppCompatActivity {
+    EditText signUpPageEmail,signUpPagePassword,signUpPageConfirmPassword;
+    Button SignUpButton;
+    DBHelper myDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        signUpPageEmail = findViewById(R.id.signUpPageEmail);
+        signUpPagePassword = findViewById(R.id.signUpPagePassword);
+        signUpPageConfirmPassword = findViewById(R.id.signUpPageConfirmPassword);
+        SignUpButton = findViewById(R.id.SignUpButton);
+
+        myDB = new DBHelper(this);
+
+        View.OnClickListener signUpButtonListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String email = signUpPageEmail.getText().toString();
+                String password = signUpPagePassword.getText().toString();
+                String confirmPassword = signUpPageConfirmPassword.getText().toString();
+
+                if(email.equals("") || password.equals("") || confirmPassword.equals("")){
+                    Toast.makeText(SignUpActivity.this,"Please Fill Up All Fields!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    if(confirmPassword.equals(password) == false){
+                        Toast.makeText(SignUpActivity.this, "Passwords do not match!", Toast.LENGTH_SHORT).show();
+                        signUpPagePassword.setText("");
+                        signUpPageConfirmPassword.setText("");
+                    }
+                    else {
+                        if(myDB.SignUpVerification(email) == true){
+                            Toast.makeText(SignUpActivity.this,"User already exists!", Toast.LENGTH_SHORT).show();
+                            signUpPageEmail.setText("");
+                            signUpPagePassword.setText("");
+                            signUpPageConfirmPassword.setText("");
+                        }
+                        else {
+                            if(myDB.insertData(email,password) == true){
+                                Toast.makeText(SignUpActivity.this,"User Registered Successfully!",Toast.LENGTH_SHORT).show();
+                                signUpPageEmail.setText("");
+                                signUpPagePassword.setText("");
+                                signUpPageConfirmPassword.setText("");
+                            }
+                            else {
+                                Toast.makeText(SignUpActivity.this,"Error! Please Try Again",Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                    }
+
+                }
+            }
+        };
+
+        SignUpButton.setOnClickListener(signUpButtonListener);
     }
 }
